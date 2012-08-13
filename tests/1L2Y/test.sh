432994b9b37e92d511ecc2932421db9626231435
Dan Sindhikara <sindhikara@gmail.com>
2012-08-13 16:00:44 +0900

    Enhanced test shell script for clarity

--- tests/1L2Y/test.sh
+++ tests/1L2Y/test.sh
@@ -1,3 +1,14 @@
 rm test/*pdb
+echo "Running local placevent installation..."
 python ../../placevent.py 1L2Y.O.1.dx 55.5 > test/O.pdb
-diff test/O.pdb ref/O.pdb
+echo "Printing difference between local test output and reference to file 'diffs'"
+diff test/O.pdb ref/O.pdb > diffs
+anydiffs=`wc -l diffs | awk '{print $1}'`;
+if [ $anydiffs -gt 0 ] 
+then
+    echo "!! Differences found! Please check diffs file!"
+    echo "Try downloading the latest version on: https://github.com/dansind/Placevent"
+    echo "If this still fails, please contact me at sindhikara@gmail.com"
+else 
+    echo "No differences found! :)"
+fi
