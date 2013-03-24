1c7831659ef4eff1b484222af9a6dc54d4eb49e5
Dan Sindhikara <sindhikara@gmail.com>
2013-03-24 12:23:59 +0900

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ config = {
     'url':'http://dansindhikara.com/Software/Entries/2012/6/22_Placevent_New.html',
     'download_url': 'http://dansindhikara.com/Software/Entries/2012/6/22_Placevent_New.html',
     'author_email': 'sindhikara@gmail.com',
-    'version': '1.2.2',
+    'version': '1.3',
     'install_requires': ['numpy'],
     'packages': ['placevent', 'placevent.tests', 'pgrid', 'ppdb'],
     'package_data': {'placevent.tests': ['1L2Y/diffs','1L2Y/*.*', '1L2Y/ref/*'],
